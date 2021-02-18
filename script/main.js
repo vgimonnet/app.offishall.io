3d120f5f0f2d056a0c4bbab25eb52e6b905f85ed
VGimonnet <valentin.gimonnet18@gmail.com>
2021-02-18 19:20:30 +0100

    Ajout page dashboard

--- script/main.js
+++ script/main.js
@@ -5,23 +5,30 @@ document.addEventListener('DOMContentLoaded', () => {
         nav = JSON.parse(localStorage.getItem('nav'));
         Array.from(nav).forEach((element, index) => {
             let span = document.getElementById(element[0]);
-            if(span.dataset.premium == 'false' || (span.dataset.premium == 'true' && window.location.href.includes('premium.html'))){
-                let ul = document.getElementById('aside-nav');
-                let li = document.createElement('li');
-                let a = document.createElement('a');
-                a.href = "#";
-                a.textContent = element[1];
-                a.className += 'nav-link';
-                li.className += element[0];
-                li.append(a);
-                ul.append(li);
-                document.getElementById('my-functionality-container').append(span);
-            } else {
-                nav.splice(index, 1);
-                localStorage.setItem('nav', JSON.stringify(nav));
+            if(span != null) {
+                if(span.dataset.premium == 'false' || (span.dataset.premium == 'true' && window.location.href.includes('premium.html'))){
+                    // let ul = document.getElementById('aside-nav');
+                    // let li = document.createElement('li');
+                    // let a = document.createElement('a');
+                    // a.href = "#";
+                    // a.textContent = element[1];
+                    // a.className += 'nav-link';
+                    // li.className += element[0];
+                    // li.append(a);
+                    // ul.append(li);
+                    document.getElementById('my-functionality-container').append(span);
+                } else {
+                    nav.splice(index, 1);
+                    localStorage.setItem('nav', JSON.stringify(nav));
+                }
             }
+            
         });
     }
+    document.getElementById('add-functionality').addEventListener('click', event => {
+        // document.getElementById('modalFunctionality').style.display = 'block';
+        // $('#modalFunctionality').show();
+    })
 })
 
 
@@ -42,24 +49,24 @@ function onDrop(event) {
         .dataTransfer
         .clearData();
     if(dropzone.id == 'my-functionality-container') {
-        let ul = document.getElementById('aside-nav');
-        let li = document.createElement('li');
-        let a = document.createElement('a');
-        a.href = "#";
-        a.textContent = draggableElement.textContent;
-        a.className += 'nav-link';
-        li.className += draggableElement.id;
+        // let ul = document.getElementById('aside-nav');
+        // let li = document.createElement('li');
+        // let a = document.createElement('a');
+        // a.href = "#";
+        // a.textContent = draggableElement.textContent;
+        // a.className += 'nav-link';
+        // li.className += draggableElement.id;
         let idLi = draggableElement.id;
         let textContent = draggableElement.textContent; 
-        li.append(a);
-        ul.append(li);
+        // li.append(a);
+        // ul.append(li);
         nav.push([idLi, textContent]);
         localStorage.setItem('nav', JSON.stringify(nav));
     } else if(dropzone.id == 'functionality-container') {
-        let lis = document.getElementsByClassName(draggableElement.id);
-        Array.from(lis).forEach(li => {
-            li.remove();
-        });
+        // let lis = document.getElementsByClassName(draggableElement.id);
+        // Array.from(lis).forEach(li => {
+        //     li.remove();
+        // });
         Array.from(nav).forEach((element, index) => {
             if (element[0] == draggableElement.id) {
                 nav.splice(index, 1);
---
+++ script/modal.js
@@ -0,0 +1,78 @@
+var modal = document.getElementById("modalFunctionality");
+
+// Get the button that opens the modal
+var btn = document.getElementById("modalOpen");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  let myFunctionalities = JSON.parse(localStorage.getItem('nav'));
+  let modalBody = document.getElementById('modal-body');
+  if (myFunctionalities != null) {
+    let ul = document.createElement('ul');
+    ul.className += 'custom-list';
+    Array.from(nav).forEach((element, index) => {
+      let text = element[1];
+      let id = element[0];
+      let li = document.createElement('li');
+      li.className += 'custom-list-item';
+      li.id = id;
+      li.textContent = text;
+      ul.append(li);
+    });
+    modalBody.innerHTML = '';
+    modalBody.append(ul);
+    Array.from(document.getElementsByClassName("custom-list-item")).forEach(list => {
+      list.addEventListener('click', event => {
+        let listItem = event.currentTarget;
+        setListItemActive(listItem)
+        // console.log(event.currentTarget);
+      });
+    });
+  }
+  
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+document.getElementById('add-functionality').addEventListener('click', event => {
+  let selectedItem = document.getElementsByClassName('item-active')[0];
+  let block = document.createElement('div');
+  block.classList.add('block');
+  block.classList.add('col-6');
+  let buttonDelete = document.createElement('button');
+  buttonDelete.textContent = 'x';
+  buttonDelete.classList.add('btn-delete-block');
+  buttonDelete.addEventListener('click', event => {
+    event.target.parentNode.remove();
+  })
+  let span = document.createElement('span');
+  span.classList.add('block-label');
+  span.textContent = selectedItem.textContent;
+  block.append(buttonDelete);
+  block.append(span);
+  document.getElementById('block-container').append(block);
+  modal.style.display = "none";
+});
+
+function setListItemActive(listItem)
+{
+  let activeItems = document.getElementsByClassName('item-active');
+  for (let i = 0; i < activeItems.length; i++) {
+    activeItems[i].classList.remove('item-active');
+  }
+  listItem.classList.add('item-active');
+}
