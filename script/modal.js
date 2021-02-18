a99b8625dab9ccaf3be28f87ccc89423c3753886
VGimonnet <valentin.gimonnet18@gmail.com>
2021-02-18 23:03:14 +0100

    Ajout d'une notification sur le dashboard

--- script/modal.js
+++ script/modal.js
@@ -1,3 +1,45 @@
+let first = true;
+document.addEventListener('DOMContentLoaded', () => {
+  showNotif();
+  clearNotif();
+});
+
+function showNotif() {
+  if(!first) {
+    console.log(first);
+    let notif = document.createElement('div');
+    notif.classList.add('notif');
+    notif.innerHTML = `
+      <img src="./public/images/coffee.png" alt="Logo img" class="notif-img">
+      <div class="notif-body">
+        <button class="closeNotif">
+          <img src="./public/images/close.png" alt="Logo close">
+        </button>
+        <p class="notif-text">Pensez à prendre des pauses régulièrement !</p>
+        <span>
+          <a href="#" class="notif-link">Rejoindre une break room</a>
+          <img src="./public/images/arrow.png">
+        </span>
+      </div>
+    `;
+    document.getElementById('notif-container').append(notif);
+    document.getElementsByClassName('closeNotif')[0].addEventListener('click', event => {
+      console.log(event.currentTarget.parentNode.parentNode.remove());
+    });
+    setTimeout("clearNotif()", 5000);
+  } else {
+    first = false;
+  }
+  setTimeout("showNotif()",20000);
+}
+
+function clearNotif() {
+  let notifs = document.getElementsByClassName('notif');
+  for(let i = 0; i < notifs.length; i++) {
+    notifs[i].remove();
+  }
+}
+
 var modal = document.getElementById("modalFunctionality");
 
 // Get the button that opens the modal
